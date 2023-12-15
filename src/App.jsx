b45a4967a245f0c34f86d4ae34d15219d9346edb
sonata22 <nataliia.sosnovshchenko@gmail.com>
2023-12-15 20:04:19 +0200

    fixed not changing cursor upon nft img hover

--- src/App.jsx
+++ src/App.jsx
@@ -177,6 +177,7 @@ const NftComponent = (nftData) => {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    cursor: cursor,
   };
   const nftTemplate = {
     width: "220px",
@@ -194,8 +195,8 @@ const NftComponent = (nftData) => {
       <div style={nftTemplate}>
         <div
           style={imgWithHoverStyle}
-          onMouseEnter={() => { setEyeIconOpacity("1"); setOpacity("0.7"); }}
-          onMouseLeave={() => { setEyeIconOpacity("0"); setOpacity("0"); }}
+          onMouseEnter={() => { setEyeIconOpacity("1"); setOpacity("0.7"); setCursor("pointer"); }}
+          onMouseLeave={() => { setEyeIconOpacity("0"); setOpacity("0"); setCursor("default"); }}
         >
           <div style={nftImgHoverBg}></div>
           <svg style={eyeIconStyle} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M15 12c0 1.654-1.346 3-3 3s-3-1.346-3-3 1.346-3 3-3 3 1.346 3 3zm9-.449s-4.252 8.449-11.985 8.449c-7.18 0-12.015-8.449-12.015-8.449s4.446-7.551 12.015-7.551c7.694 0 11.985 7.551 11.985 7.551zm-7 .449c0-2.757-2.243-5-5-5s-5 2.243-5 5 2.243 5 5 5 5-2.243 5-5z" fill="#ffffff" /></svg>
